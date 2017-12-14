477f54756743661532381f67adb6346bf94d9cea
Jinwoo Bae <bbae7050@gmail.com>
2017-12-14 20:15:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = () => {
   return ['snickers', 'hundred grand', 'kitkat', 'skittles']
 }
 
-var addElementToBeginningOfArray = (array, element) => {return [element, ...array]
+var addElementToBeginningOfArray = (array, element) => {return [element, ...array]}
 
 var descructivelyAddElementToBeginningOfArray = (array, element) => {
   array.unshift(element);
